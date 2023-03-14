8961ad6edcf5425787fa3ed91a5b358c3fdfd08e
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-03-14 10:06:40 +0700

    Build Script outputs config file,  output clearer

--- scripts/build-circuits.sh
+++ scripts/build-circuits.sh
@@ -2,43 +2,58 @@
 set -e
 
 cd "$(dirname "$0")"
-
+zkeypath="../zkeyFiles"
 mkdir -p ../build/contracts
 mkdir -p ../build/setup
-mkdir -p ../build/zkeyFiles
+mkdir -p $zkeypath
 
 # Build context
 cd ../build
-
+echo -e "\033[36m----------------------\033[0m"
+echo -e "\033[36mSETTING UP ENVIRONMENT\033[0m"
+echo -e "\033[36m----------------------\033[0m"
 if [ -f ./powersOfTau28_hez_final_14.ptau ]; then
-    echo "powersOfTau28_hez_final_14.ptau already exists. Skipping."
+    echo -e "\033[33mpowersOfTau28_hez_final_14.ptau already exists. Skipping.\033[0m"
 else
-    echo "Downloading powersOfTau28_hez_final_14.ptau"
+    echo -e "\033[33mDownloading powersOfTau28_hez_final_14.ptau\033[0m"
     wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_14.ptau
 fi
 
-circuit_path="../circuits/rln-same.circom"
-
-echo "$PWD"
+circuit_path=""
+circuit_type=""
+if [ "$1" = "diff" ]; then
+    echo -e "\033[32mUsing Diff circuit\033[0m"
+    circuit_type="diff"
+    circuit_path="../circuits/rln-diff.circom"
+elif [ "$1" = "same" ]; then
+    echo -e "\033[32mUsing Same circuit\033[0m"
+    circuit_type="same"
+    circuit_path="../circuits/rln-same.circom"
+else
+    circuit_type="same"
+    circuit_path="../circuits/rln-same.circom"
+    echo -e "\033[33mUnrecognized argument, using 'same' as default.\033[0m"
+fi
 
 if ! [ -x "$(command -v circom)" ]; then
-    echo 'Error: circom is not installed.' >&2
-    echo 'Error: please install circom: https://docs.circom.io/getting-started/installation/.' >&2
+    echo -e '\033[31mError: circom is not installed.\033[0m' >&2
+    echo -e '\033[31mError: please install circom: https://docs.circom.io/getting-started/installation/.\033[0m' >&2
     exit 1
 fi
 
-echo "Circuit path: $circuit_path"
+echo -e "Circuit path: $circuit_path"
+echo -e "\033[36m-----------------\033[0m"
+echo -e "\033[36mCOMPILING CIRCUIT\033[0m"
+echo -e "\033[36m-----------------\033[0m"
 
-which -a circom
-echo "-----------------"
-echo "COMPILING CIRCUIT"
-echo "-----------------"
+echo -e "\033[36m Build Path: $PWD\033[0m"
+
 circom --version
 circom $circuit_path --r1cs --wasm --sym
 
 snarkjs r1cs export json rln-same.r1cs rln-same.r1cs.json
 
-echo "Running groth16 trusted setup"
+echo -e "\033[36mRunning groth16 trusted setup\033[0m"
 
 snarkjs groth16 setup rln-same.r1cs powersOfTau28_hez_final_14.ptau setup/rln_0000.zkey
 
@@ -46,10 +61,16 @@ snarkjs zkey contribute setup/rln_0000.zkey setup/rln_0001.zkey --name="First co
 snarkjs zkey contribute setup/rln_0001.zkey setup/rln_0002.zkey --name="Second contribution" -v -e="Another random entropy"
 snarkjs zkey beacon setup/rln_0002.zkey setup/rln_final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
 
-echo "Exporting artifacts to zkeyFiles and contracts directory"
+echo -e "Exporting artifacts to zkeyFiles and contracts directory"
 
-snarkjs zkey export verificationkey setup/rln_final.zkey zkeyFiles/verification_key.json
+snarkjs zkey export verificationkey setup/rln_final.zkey $zkeypath/verification_key.json
 snarkjs zkey export solidityverifier setup/rln_final.zkey contracts/verifier.sol
 
-cp rln-same_js/rln-same.wasm zkeyFiles/rln-same.wasm
-cp setup/rln_final.zkey zkeyFiles/rln_final.zkey
+cp rln-same_js/rln-same.wasm $zkeypath/rln-same.wasm
+cp setup/rln_final.zkey $zkeypath/rln_final.zkey
+
+echo -e "RLN_Version: V2" > $zkeypath/circuit.config
+echo -e "RLN_Type: $circuit_type" >> $zkeypath/circuit.config
+echo -e "GitHub_URL: $(git config --get remote.origin.url)"  >> $zkeypath/circuit.config
+echo -e "Git_Commit: $(git describe --always)" >> $zkeypath/circuit.config
+echo -e "Compilation_Time: $(date +%s)" >> $zkeypath/circuit.config
